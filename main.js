850ac99769eb97b3208435d54aa88b328c068f63
ofylypets <ostapfull1@gmail.com>
2024-02-22 09:00:05 +0200

    Lesson1

---
+++ main.js
@@ -0,0 +1,45 @@
+let stringOne='hello';
+let stringTwo='owu';
+let stringThree='com';
+let stringFour='ua';
+let numberOne=1;
+let numberTwo=10;
+let numberThree=-999;
+let numberFour=123;
+let numberFive=3.14;
+let numberSix=2.7;
+let numberSeven=16;
+let booleanOne=true;
+let booleanTwo=false;
+console.log(stringOne);
+console.log(stringTwo);
+console.log(stringThree);
+console.log(stringFour);
+console.log(numberOne);
+console.log(numberTwo);
+console.log(numberThree);
+console.log(numberFour);
+console.log(numberFive);
+console.log(numberSix);
+console.log(numberSeven);
+console.log(booleanOne);
+console.log(booleanTwo);
+// --------------------------------------------------
+let firstName='Ostap';
+let secondName='Myroslavovych';
+let lastName='Fylypets';
+let person = `${firstName} ${secondName} ${lastName}`;
+console.log(person);
+// --------------------------------------------------
+let a= 100;
+let b= '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+// --------------------------------------------------
+ let name = prompt('Як тебе звати?');
+let lastname = prompt('Яка в тебе фамілія?');
+let age = prompt('Скільки тобі років?')
+let personTwo = `${name} ${lastname} ${age}`
+console.log(personTwo)
